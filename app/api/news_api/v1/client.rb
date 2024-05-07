4aac957a8aa69d7addb3869ea88e5265784fed23
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-07 16:00:43 +0800

    feat: add endpoint method and private request method

--- app/api/news_api/v1/client.rb
+++ app/api/news_api/v1/client.rb
@@ -6,28 +6,45 @@ class NewsApi::V1::Client
   API_BASE_URL = 'https://newsapi.org/v2'
   API_KEY = Rails.application.credentials.news_api.api_key
 
+  # endpoint method
+  def everything(q = '', **params)
+    request(
+      http_method: :get,
+      endpoint: 'everything',
+      params: { q: q, **params }
+    )
+  end
+
+  # add more endpoint methods as needed
+
   private
 
   def client
     @client ||= begin
       options = {
         request: {
-          # modify timeout behavior in sec (faraday default = 60sec)
-          open_timeout: 10, # if connection is not established within said time, req will timeout and raise an error
-          read_timeout: 10 # if connection is not read within said time, req will timeout and raise an error
+          open_timeout: 10,
+          read_timeout: 10
         }
       }
-
-      # configure http client instance using faraday middlewares
-      # ref: https://lostisland.github.io/faraday/#/middleware/index?id=available-middleware
       Faraday.new(url: API_BASE_URL, **options) do |conn|
         conn.request :authorization, 'Bearer', API_KEY
         conn.request :json
-        conn.response :json, parser_options: { symbolize_names: true } # ref: https://github.com/lostisland/faraday_middleware/wiki/Parsing-responses
-        conn.response :raise_error # raise Faraday::Error on status code 4xx or 5xx
-        conn.response :logger, Rails.logger, headers: true, bodies: true, log_level: :debug # ref: https://www.rubydoc.info/github/lostisland/faraday/Faraday/Logging/Formatter
+        conn.response :json, parser_options: { symbolize_names: true }
+        conn.response :raise_error
+        conn.response :logger, Rails.logger, headers: true, bodies: true, log_level: :debug
       end
     end
+  end
+
+  def request(http_method:, endpoint:, params: {}) # add 'body: {}' if you'll have a post/patch http method (check endpoint method requirements)
+    response = client.public_send(http_method, endpoint, params) # add 'body' if you'll have a post/patch http method
+    {
+      status: response.status,
+      body: response.body
+    }
+  rescue Faraday::Error => error
+    puts error.inspect
   end
 
 end
