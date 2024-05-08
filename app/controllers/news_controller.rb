88d15ed00812faa3584ef309e848420f49231dd2
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-08 12:53:02 +0800

    refactor: make query dynamic for everything endpoint call

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,18 +1,17 @@
 class NewsController < ApplicationController
 
   def index
+    @query = params[:query].presence
     news = NewsApi::V1::Client.new
-    res = news.everything('Ruby on Rails')
+    res = news.everything(@query, sortBy: 'relevancy', language: 'en', pageSize: 10)
 
     if res[:status] == 200
       @articles = res[:body][:articles]
-    else
-      flash[:error] = "Failed to fetch news articles: #{res[:body]['message']}"
-      @articles = []
     end
 
   rescue Errors::ApiError => error
     flash[:error] = error.message.capitalize
     @articles = []
   end
+
 end
