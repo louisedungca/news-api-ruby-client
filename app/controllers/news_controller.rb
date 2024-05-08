223adec7656b282cdb3cc729ecdf186ee7e6caa9
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-08 15:56:18 +0800

    refactor: modify index action and add top_headlines api call

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,19 +1,24 @@
 class NewsController < ApplicationController
   def index
     @query = params[:query].presence
+    return @articles = [] unless @query.present?
 
-    if @query.present?
-      news = NewsApi::V1::Client.new
-      res = news.everything(@query, sortBy: 'relevancy', language: 'en', pageSize: 10)
+    news = NewsApi::V1::Client.new
+    all_news = news.everything(@query, sortBy: 'relevancy', language: 'en', pageSize: 20)
+    top_news = news.top_headlines(country: 'ph', pageSize: 15)
 
-      if res[:status] == 200
-        @articles = res[:body][:articles]
-      else
-        flash[:error] = "Failed to fetch news articles. #{res[:body]['message'.capitalize]}"
-        @articles = []
-      end
+    if all_news[:status] == 200
+      @articles = all_news[:body][:articles]
     else
+      flash[:error] = "Failed to fetch news articles. #{all_news[:body]['message'.capitalize]}"
       @articles = []
+    end
+
+    if top_news[:status] == 200
+      @top_articles = top_news[:body][:articles]
+    else
+      flash[:error] = "Failed to fetch news articles. #{top_news[:body]['message'.capitalize]}"
+      @top_articles = []
     end
 
   rescue Errors::ApiError => error
