9c1ba45a03ba0c45d7accff98b40e6ce2b6f9d36
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-08 13:23:07 +0800

    fix: fix auto api call on reload bug

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,17 +1,23 @@
 class NewsController < ApplicationController
-
   def index
     @query = params[:query].presence
-    news = NewsApi::V1::Client.new
-    res = news.everything(@query, sortBy: 'relevancy', language: 'en', pageSize: 10)
 
-    if res[:status] == 200
-      @articles = res[:body][:articles]
+    if @query.present?
+      news = NewsApi::V1::Client.new
+      res = news.everything(@query, sortBy: 'relevancy', language: 'en', pageSize: 10)
+
+      if res[:status] == 200
+        @articles = res[:body][:articles]
+      else
+        flash[:error] = "Failed to fetch news articles. #{res[:body]['message'.capitalize]}"
+        @articles = []
+      end
+    else
+      @articles = []
     end
 
   rescue Errors::ApiError => error
     flash[:error] = error.message.capitalize
     @articles = []
   end
-
 end
